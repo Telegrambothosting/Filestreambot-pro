d88d3d12d68c0757529fc7d5c6c43e442bb0cec2
Telegrambothosting <122618897+Telegrambothosting@users.noreply.github.com>
2023-01-29 21:37:58 +0530

    Add files via upload

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -1,4 +1,4 @@
-# (c) adarsh-goel
+# DARK DEVIL
 import os
 from os import getenv, environ
 from dotenv import load_dotenv
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    name = str(getenv('name', 'filetolinkbot'))
+    API_ID = int(getenv('19562731'))
+    API_HASH = str(getenv('1667473e722f17eaa8ccbb98dc5f727d'))
+    BOT_TOKEN = str(getenv('5658141178:AAEkBB3Q0PP1pm-5PpaklZeJr4BmGRj3GyA'))
+    name = str(getenv('name', 'File-To-Url'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    BIN_CHANNEL = int(getenv('-1001756916904'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    OWNER_USERNAME = str(getenv('DARKDevilV2'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
+    DATABASE_URL = str(getenv('mongodb+srv://file-to-url:dark-devil@cluster0.8hwy07p.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('DarkDevilBotz', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
